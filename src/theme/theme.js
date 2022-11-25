205d4f67f68d912e89e7cf66206de23a50ef16d1
Julio Duarte <73315417+maoiki@users.noreply.github.com>
2022-11-25 20:50:35 -0300

    Changed theme colors

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -4,7 +4,9 @@ export const theme = {
     fundo_secundario: '#F4F4F5',
     texto_principal: '#F4F4F5',
     texto_secundario: '#F4F4F5',
-    detalhes: '#F4F4F5',
-    branco: '#F4F4F5',
+    details_green: '#068d9d',
+    details_orange:'#d59b5d',
+    white: '#F5F5F5',
+    black: '#272932',
   },
 };
